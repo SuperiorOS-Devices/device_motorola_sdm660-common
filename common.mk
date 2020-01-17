ccbf96243ac85c568e9585d8dd15efaad4ba2f83
trpsl <trpsl755@gmail.com>
2020-01-17 02:29:20 +0100

    sdm660: convert LineageActions to MotoActions
    Signed-off-by: trpsl <trpsl755@gmail.com>

--- common.mk
+++ common.mk
@@ -213,9 +213,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.sdm660
 
-# LineageActions
+# MotoActions
 PRODUCT_PACKAGES += \
-    LineageActions
+    MotoActions
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
